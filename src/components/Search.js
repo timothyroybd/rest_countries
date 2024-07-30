1313820e06b0a5e57097d340f0ece74e2c277c6f
Timothy Roy <timothyroy94@protonmail.com>
2024-07-30 18:39:45 +0200

    sorting by continent added

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,30 +2,36 @@ import '../styles/Search.css';
 import { Container, Row, Col } from 'react-bootstrap';
 
 const Search = ({ data, setData, originalData }) => {
+  const searchHandler = (e) => {
+    const value = e.target.value.toLowerCase().trim();
+    console.log('Search input:', value);
 
- 
+    if (value === '') {
+      setData(originalData);
+      console.log('Reverted to original data:', data);
+    } else {
+      const searchedArray = originalData.filter((element) => {
+        const name = element.name.common.toLowerCase();
+        return name.startsWith(value);
+      });
+      setData(searchedArray);
+      console.log('Filtered data:', searchedArray);
+    }
+  };
+  const selectHandler = (e) => {
+    const value = e.target.value.toLowerCase();
+    console.log(value);
+    if (value === '') {
+      setData(originalData);
+    } else {
+      const filterArray = originalData.filter((element) => {
+        const region = element.region.toLowerCase();
 
-  const changeHandler = (e) => {
-  
-
-  const value = e.target.value.toLowerCase().trim();
-  console.log("Search input:", value);
-
-  if(value === ''){
-    
-    setData(originalData)
-    console.log("Reverted to original data:", data);
-  } else {
-    const searchedArray = originalData.filter(element => {
-      const name = element.name.common.toLowerCase();
-      return name.startsWith(value)
-    })
-    setData(searchedArray)
-     console.log("Filtered data:", searchedArray);
-  }
-
-  
-};
+        return region === value;
+      });
+      setData(filterArray);
+    }
+  };
 
   return (
     <Container>
@@ -34,13 +40,14 @@ const Search = ({ data, setData, originalData }) => {
           className="item"
           type="text"
           placeholder="Search"
-          onChange={(e) => changeHandler(e, data, setData)}
+          onChange={(e) => searchHandler(e, data, setData)}
         />
 
-        <select className="item">
+        <select className="item" onChange={selectHandler}>
           <option value="">Filter by Region</option>
           <option value="africa">Africa</option>
-          <option value="america">America</option>
+          <option value="americas">America</option>
+          <option value="antarctic">Antarctic</option>
           <option value="asia">Asia</option>
           <option value="europe">Europe</option>
           <option value="oceania">Oceania</option>
